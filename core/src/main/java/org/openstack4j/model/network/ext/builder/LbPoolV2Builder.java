1b362535807c7868330b0cdd491df720c132ce9e
Allen Yin <allen.yin@sap.com>
2020-06-05 22:22:16 +0800

    fix format style

--- core/src/main/java/org/openstack4j/model/network/ext/builder/LbPoolV2Builder.java
+++ core/src/main/java/org/openstack4j/model/network/ext/builder/LbPoolV2Builder.java
@@ -10,11 +10,13 @@ import org.openstack4j.model.network.ext.SessionPersistence;
  * A Builder to create a lbpoolV2
  *
  * @author emjburns
+ *
  */
 public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPoolV2> {
     /**
-     * @param tenantId Owner of the pool. Only an administrative user can specify a
-     *                 tenant ID other than its own.
+     * @param tenantId
+     *            Owner of the pool. Only an administrative user can specify a
+     *            tenant ID other than its own.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder tenantId(String tenantId);
@@ -22,7 +24,8 @@ public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPo
     /**
      * Optional
      *
-     * @param name Pool name. Does not have to be unique.
+     * @param name
+     *            Pool name. Does not have to be unique.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder name(String name);
@@ -30,24 +33,27 @@ public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPo
     /**
      * Optional
      *
-     * @param description Description for the pool.
+     * @param description
+     *            Description for the pool.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder description(String description);
 
     /**
-     * @param protocol The protocol of the VIP address. A valid value is TCP, HTTP,
-     *                 or HTTPS.
+     * @param protocol
+     *            The protocol of the VIP address. A valid value is TCP, HTTP,
+     *            or HTTPS.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder protocol(Protocol protocol);
 
     /**
-     * @param lbMethod The load-balancer algorithm, which is round-robin,
-     *                 least-connections, and so on. This value, which must be
-     *                 supported, is dependent on the load-balancer provider. Round
-     *                 robin must be supported.
-     *                 Must be one of ROUND_ROBIN, LEAST_CONNECTIONS, or SOURCE_IP.
+     * @param lbMethod
+     *            The load-balancer algorithm, which is round-robin,
+     *            least-connections, and so on. This value, which must be
+     *            supported, is dependent on the load-balancer provider. Round
+     *            robin must be supported.
+     *            Must be one of ROUND_ROBIN, LEAST_CONNECTIONS, or SOURCE_IP.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder lbMethod(LbMethod lbMethod);
@@ -55,7 +61,8 @@ public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPo
     /**
      * Optional
      *
-     * @param sessionPersistence Default value empty dictionary
+     * @param sessionPersistence
+     *            Default value empty dictionary
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder sessionPersistence(SessionPersistence sessionPersistence);
@@ -63,8 +70,9 @@ public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPo
     /**
      * Optional
      *
-     * @param adminStateUp The administrative state of the lb pool, which is up (true) or
-     *                     down (false). Default value true.
+     * @param adminStateUp
+     *            The administrative state of the lb pool, which is up (true) or
+     *            down (false). Default value true.
      * @return LbPoolV2Builder
      */
     LbPoolV2Builder adminStateUp(boolean adminStateUp);
@@ -72,7 +80,6 @@ public interface LbPoolV2Builder extends Buildable.Builder<LbPoolV2Builder, LbPo
     /**
      * The listener in which this pool will become the default pool.
      * There can only be on default pool for a listener.
-     *
      * @param listenerId
      * @return LbPoolV2Builder
      */
